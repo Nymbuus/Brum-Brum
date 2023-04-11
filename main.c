1bd0a63a6eca1f2cf06b0b49e9f4adaecb108cb2
ayadinho <116158263+ayadinho@users.noreply.github.com>
2023-04-11 16:12:24 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "header.h"
+
+
+int main(void)
+{
+	DDRB  = (1 << LED1);
+	PORTB = (1 << REMOTESTART);
+	
+	while (1)
+	{
+		if (REMOTESTART_PRESSED)
+		{
+			LED1_ON;
+		}
+		else
+		{
+			LED1_OFF;
+		}
+	}
+}
+
